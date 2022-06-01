7afd8b383c5fb47c3c0eb2edefcf2ef68ce52935
Alexis <alexislecuyer14@gmail.com>
2022-06-01 09:56:13 +0200

    file modif

--- filebeat-install.sh
+++ filebeat-install.sh
@@ -10,7 +10,7 @@ curl -so /etc/filebeat/filebeat.yml https://packages.wazuh.com/4.3/tpl/elastic-b
 curl -so /etc/filebeat/wazuh-template.json https://raw.githubusercontent.com/wazuh/wazuh/4.3/extensions/elasticsearch/7.x/wazuh-template.json
 chmod go+r /etc/filebeat/wazuh-template.json
 curl -s https://packages.wazuh.com/4.x/filebeat/wazuh-filebeat-0.2.tar.gz | tar -xvz -C /usr/share/filebeat/module
-output.elasticsearch.password: <elasticsearch_password>
+echo "output.elasticsearch.password: <elasticsearch_password>" >> /etc/filebeat/filebeat.yml
 cp -r /etc/elasticsearch/certs/ca/ /etc/filebeat/certs/
 cp /etc/elasticsearch/certs/elasticsearch.crt /etc/filebeat/certs/filebeat.crt
 cp /etc/elasticsearch/certs/elasticsearch.key /etc/filebeat/certs/filebeat.key
