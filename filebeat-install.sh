88f6676676100848aece8e05264a10cc14e7ef5e
Alexis <alexislecuyer14@gmail.com>
2022-06-01 09:53:40 +0200

    filebeat

---
+++ filebeat-install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt-get install filebeat=7.17.3
+curl -so /etc/filebeat/filebeat.yml https://packages.wazuh.com/4.3/tpl/elastic-basic/filebeat_all_in_one.yml
+curl -so /etc/filebeat/wazuh-template.json https://raw.githubusercontent.com/wazuh/wazuh/4.3/extensions/elasticsearch/7.x/wazuh-template.json
+chmod go+r /etc/filebeat/wazuh-template.json
+curl -s https://packages.wazuh.com/4.x/filebeat/wazuh-filebeat-0.2.tar.gz | tar -xvz -C /usr/share/filebeat/module
+output.elasticsearch.password: <elasticsearch_password>
+cp -r /etc/elasticsearch/certs/ca/ /etc/filebeat/certs/
+cp /etc/elasticsearch/certs/elasticsearch.crt /etc/filebeat/certs/filebeat.crt
+cp /etc/elasticsearch/certs/elasticsearch.key /etc/filebeat/certs/filebeat.key
+systemctl daemon-reload
+systemctl enable filebeat
+systemctl start filebeat
+filebeat test output
